463ce70662ea8e9a702681a012142fe3e5be80aa
Vinay Konatala <vinay.konatala@gmail.com>
2022-06-06 16:04:57 +0530

    UI updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,9 +7,18 @@ import {
   Stack,
   Link,
   Heading,
+  Caption,
+  DropZone,
+  Thumbnail,
+  Button,
+  Checkbox,
+  Form,
+  FormLayout,
+  TextField,
 } from "@shopify/polaris";
+import { NoteMinor } from "@shopify/polaris-icons";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import trophyImgUrl from "../assets/home-trophy.png";
 
@@ -17,9 +26,12 @@ import { ProductsCard } from "./ProductsCard";
 
 export function HomePage() {
   const [todoList, setTodoLisr] = useState([]);
+  const [newsletter, setNewsletter] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [email, setEmail] = useState("");
   const getTodoList = async () => {
     try {
-      const res = await axios.get(`https://reqres.in/api/users?page=${1}`)
+      const res = await axios.get(`https://reqres.in/api/users?page=${1}`);
       // .then((res) => { console.log('res', res) })
       // .catch((error) => {
       //     console.error(error);
@@ -28,62 +40,179 @@ export function HomePage() {
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  console.log("todoList", todoList)
+  console.log("todoList", todoList);
 
   useEffect(() => {
-    const fetchData = (() => {
+    const fetchData = () => {
       getTodoList();
-    })
+    };
     fetchData();
   }, []);
+
+  const handleChange = useCallback((value) => setEmail(value), []);
+
+  const handleSubmit = useCallback((_event) => {
+    setEmail("");
+    setNewsletter(false);
+    console.log(_event);
+  }, []);
+
+  const handleDropZoneDrop = useCallback(
+    (_dropFiles, acceptedFiles, _rejectedFiles) =>
+      setFiles((files) => [...files, ...acceptedFiles]),
+    []
+  );
+
+  const validImageTypes = ["image/gif", "image/jpeg", "image/png, csv"];
+
+  const fileUpload = !files.length && <DropZone.FileUpload />;
+  const uploadedFiles = files.length > 0 && (
+    <div style={{ padding: "0" }}>
+      <Stack vertical>
+        {files.map((file, index) => (
+          <Stack alignment="center" key={index}>
+            <Thumbnail
+              size="small"
+              alt={file.name}
+              source={
+                validImageTypes.includes(file.type)
+                  ? window.URL.createObjectURL(file)
+                  : NoteMinor
+              }
+            />
+            <div>
+              {file.name} <Caption>{file.size} bytes</Caption>
+            </div>
+          </Stack>
+        ))}
+      </Stack>
+    </div>
+  );
+
   return (
     <Page fullWidth>
       <Layout>
         <Layout.Section>
           <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>welcome to Shyft App 🎉</Heading>
-                  <table>
-                    <tr>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Email</th>
-                    </tr>
-                    {todoList.map((item) => (
-                      <tr>
-                        <td>{item.first_name}</td>
-                        <td>{item.last_name}</td>
-                        <td>{item.email}</td>
-                      </tr>
-                    ))}
+            <Form onSubmit={handleSubmit}>
+              <FormLayout>
+                <Heading element="h1">Delivery Details</Heading>
 
-                  </table>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImgUrl}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
+                <Layout>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Customer Name *"
+                      type="text"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Email Id *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Contact Number *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Area-Emirate *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Full Postal Address *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Booking Reference *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Date & Slot *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="No. of Items *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Item Description *"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                  <Layout.Section oneThird>
+                    <TextField
+                      value={email}
+                      onChange={handleChange}
+                      label="Cash to be collected (AED)"
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </Layout.Section>
+                </Layout>
+
+                <Button submit primary>
+                  Place Booking
+                </Button>
+              </FormLayout>
+
+              <div style={{ margin: "20px 0" }}>OR</div>
+
+              <FormLayout>
+                <Heading element="h1">Bulk Upload</Heading>
+                <DropZone onDrop={handleDropZoneDrop}>
+                  {uploadedFiles}
+                  {fileUpload}
+                </DropZone>
+              </FormLayout>
+            </Form>
           </Card>
         </Layout.Section>
-        <Layout.Section secondary>
+        {/* <Layout.Section secondary>
           <ProductsCard />
-        </Layout.Section>
+        </Layout.Section> */}
       </Layout>
     </Page>
   );
